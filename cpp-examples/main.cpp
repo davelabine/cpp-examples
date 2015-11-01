59e7ba673b507813063bee385d98e1c772e66cac
davelabine <davidl@smarttech.com>
2015-11-01 14:32:19 -0800

    Initial Commit

---
+++ cpp-examples/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp-examples
+//
+//  Created by David LaBine on 11/1/15.
+//  Copyright © 2015 David LaBine. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
